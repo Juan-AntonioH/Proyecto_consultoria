e47394db22f91358af06851ce19143ff49739368
Juan Antonio Herraiz <musasime4@gmail.com>
2023-06-20 11:00:17 +0200

    feat(#3):Carousel y Section info

--- pracditec/src/app/pages/inicio/inicio.component.ts
+++ pracditec/src/app/pages/inicio/inicio.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faUsers } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-inicio',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent {
-
+  users = faUsers;
 }
